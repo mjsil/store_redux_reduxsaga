dd59bb4b164b4533e23937222d20a22a51e56ab1
mjsil <m.j.silveira@outlook.com>
2020-05-25 13:53:37 -0300

    Página HOME

---
+++ src/pages/Home/index.js
@@ -0,0 +1,60 @@
+import React, { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { MdAddShoppingCart } from 'react-icons/md';
+import { formatPrice } from '../../util/format';
+import api from '../../services/api';
+
+import * as CartActions from '../../store/modules/cart/actions';
+
+import { ProductList } from './styles';
+
+export default function Home() {
+    const dispatch = useDispatch();
+    const [products, setProducts] = useState([]);
+    
+    const amount = useSelector(state => state.cart.reduce((sumAmount, product) => {
+        sumAmount[product.id] = product.amount;
+
+        return sumAmount;
+    }, {}))
+
+    function handleAddProduct(id) {
+        dispatch(CartActions.addToCartRequest(id));
+    }
+
+    useEffect(() => {
+        async function loadProducts() {
+            const response = await api.get('products');
+
+            const data = response.data.map(product => ({
+                ...product,
+                priceFormatted: formatPrice(product.price)
+            }));
+
+            setProducts(data);
+        }
+
+        loadProducts();
+    }, []);
+
+    return (
+        <ProductList>
+            {products.map(product => (
+                <li key={product.id}>
+                    <img src={product.image} alt={product.title} />
+                    <strong>{product.title}</strong>
+                    <span>{product.priceFormatted}</span>
+
+                    <button type='button' onClick={() => handleAddProduct(product.id)}>
+                        <div>
+                            <MdAddShoppingCart size={16} color='#fff' />
+                            {amount[product.id] || 0}
+                        </div>
+
+                        <span>ADICIONAR AO CARRINHO</span>
+                    </button>
+                </li>
+            ))}
+        </ProductList>
+    );
+}
